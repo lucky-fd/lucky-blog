1bd470f10f89fe90013501ed0c84c58c79210488
付东 <2522302720@qq.com>
2024-01-09 17:28:29 +0800

    修改web和file路径

---
+++ lucky-blog-file/src/main/java/com/fd/luckyblog/file/form/SearchPictureForm.java
@@ -0,0 +1,10 @@
+package com.fd.luckyblog.file.form;
+
+import com.moxi.mougblog.base.vo.FileVO;
+import lombok.Data;
+
+@Data
+public class SearchPictureForm extends FileVO {
+    private String searchKey;
+    private Integer count;
+}
---
+++ lucky-blog-file/src/main/java/com/fd/luckyblog/file/service/impl/FileSortServiceImpl.java
@@ -0,0 +1,18 @@
+package com.fd.luckyblog.file.service.impl;
+
+import com.moxi.mogublog.commons.entity.FileSort;
+import com.fd.luckyblog.file.mapper.FileSortMapper;
+import com.fd.luckyblog.file.service.FileSortService;
+import com.moxi.mougblog.base.serviceImpl.SuperServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * 服务实现类
+ *
+ * @author 陌溪
+ * @since 2018-09-17
+ */
+@Service
+public class FileSortServiceImpl extends SuperServiceImpl<FileSortMapper, FileSort> implements FileSortService {
+
+}
