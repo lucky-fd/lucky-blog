ba0c9cd824e6b5814e960832da0cea1f1a3d756b
付东 <2522302720@qq.com>
2024-01-11 15:41:50 +0800

    修改文件路径

---
+++ lucky-blog-spider/src/main/java/com/fd/luckyblog/spider/service/BlogSpiderService.java
@@ -0,0 +1,16 @@
+package com.fd.luckyblog.spider.service;
+
+import com.fd.luckyblog.spider.entity.BlogSpider;
+import com.moxi.mougblog.base.service.SuperService;
+
+/**
+ * <p>
+ * 博客爬取服务类
+ * </p>
+ *
+ * @author 陌溪
+ * @since 2020年2月7日21:28:51
+ */
+public interface BlogSpiderService extends SuperService<BlogSpider> {
+
+}
